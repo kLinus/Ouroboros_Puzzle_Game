a60e8a5dbb5b0b68bfd9618229521b0424c17388
Kris <kris@accidentallygeni.us>
2012-01-28 10:22:53 -0800

    More Burst of speed updates

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,15 +9,55 @@ public class PlayerScript : MonoBehaviour {
 	public float rotationForce;
 	public float moveForce;
 	
-	//Private variables
-	private bool burstOfSpeedEnabled = false;
+	public float burstOfSpeedMaxForce;
+	public float burstOfSpeedMultiplier;
+	public float burstOfSpeedCooldown;
+	
+	//Burst of Speed abiltiy variables
+	private bool burstOfSpeedEnabled = true;
 	private bool burstOfSpeedInUse = false;
+	private bool burstOfSpeedOnCooldown = false;
+	private float defaultMoveForce;
+	private float burstOfSpeedLastCastTime;
+	
+	//Repel ability variables
+	
+	//Straighten ability variables
 	
 	void Awake () {
 		head.gameObject.layer = LayerMask.NameToLayer("Player" + playerIndex);
+		defaultMoveForce = moveForce;
 	}
 	
 	void FixedUpdate () {
+	
+		//Burst of Speed Ability
+		if((burstOfSpeedEnabled || burstOfSpeedInUse) && !burstOfSpeedOnCooldown)
+		{
+			bool shift = (playerIndex == 1 && Input.GetKey(KeyCode.LeftShift));
+			if( shift && !burstOfSpeedInUse)
+			{
+				burstOfSpeedInUse = true;
+				burstOfSpeedEnabled = false;
+			}
+			else if (burstOfSpeedInUse)
+			{
+				moveForce *= burstOfSpeedMultiplier;
+				if(moveForce >= burstOfSpeedMaxForce)
+				{
+					moveForce = defaultMoveForce;
+					burstOfSpeedInUse = false;
+					burstOfSpeedEnabled = true;
+					burstOfSpeedLastCastTime = 
+				}
+			}
+			else
+			{
+				moveForce = defaultMoveForce;
+			}
+		}
+		
+		//Movement
 		bool left = (playerIndex == 1 && Input.GetKey(KeyCode.LeftArrow)) || (playerIndex == 2 && Input.GetKey(KeyCode.A));
 		bool right = (playerIndex == 1 && Input.GetKey(KeyCode.RightArrow)) || (playerIndex == 2 && Input.GetKey(KeyCode.D));
 		bool up = (playerIndex == 1 && Input.GetKey(KeyCode.UpArrow)) || (playerIndex == 2 && Input.GetKey(KeyCode.W));
@@ -38,9 +78,16 @@ public class PlayerScript : MonoBehaviour {
 		{
 			this.rigidbody.AddForce(Time.fixedDeltaTime * moveForce * -this.transform.forward);
 		}
+		
+		//Check cooldowns
+		
 	}
 	
-	void ToggleBurstOfSpeed() {
+	private void ToggleBurstOfSpeedEnabled() {
 		burstOfSpeedEnabled = !burstOfSpeedEnabled;
+	}
+	
+	private void ToggleBurstOfSpeedInUse() {
+		burstOfSpeedInUse = !burstOfSpeedInUse;
 	}
 }
