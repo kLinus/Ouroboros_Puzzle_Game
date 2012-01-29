417019c6d39be23dd2fe3635fc24c3510a54abab
Kris <kris@accidentallygeni.us>
2012-01-28 19:07:43 -0800

    Repel Shield Update

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,54 +9,109 @@ public class PlayerScript : MonoBehaviour {
 	public float rotationForce;
 	public float moveForce;
 	
-	public float burstOfSpeedMaxForce;
-	public float burstOfSpeedMultiplier;
-	public float burstOfSpeedCooldown;
-	public float realtime;
+	[System.Serializable]
+	public class BurstOfSpeedProperties {
+		public float maxForce;
+		public float forceMultiplier;
+		public float cooldownInSeconds;
+	}public BurstOfSpeedProperties BurstOfSpeed;
+	
+	[System.Serializable]
+	public class RepelProperties {
+		public float maxForce;
+		public float maxRange;
+		public float forceDecayMultiplier;
+		public float growthMultiplier;
+		public float cooldown;
+		public GameObject repelExplosionPrefab;
+	}public RepelProperties Repel;
+	
+	[System.Serializable]
+	public class StraightenProperties{
+		public float cooldown;
+	}public StraightenProperties Straighten;
 	
 	//Burst of Speed abiltiy variables
 	private bool burstOfSpeedEnabled = true;
 	private bool burstOfSpeedInUse = false;
-	private bool burstOfSpeedOnCooldown = false;
+	private bool burstOfSpeedIsOnCooldown = false;
 	private float defaultMoveForce;
 	private float burstOfSpeedLastCastTime = 0;
 	
 	//Repel ability variables
+	private bool repelEnabled = true;
+	private bool repelIsOnCooldown = false;
+	private float repelLastCastTime = 0;
 	
 	//Straighten ability variables
+	private bool straightenEnabled = true;
+	private bool straightenIsOnCooldown = false;
+	private float straightenLastCastTime = 0;
 	
-	void Awake () {
+	//Controls
+	private KeyCode p1_BurstOfSpeedKey = KeyCode.RightShift;
+	private KeyCode p1_RepelKey = KeyCode.Slash;
+	private KeyCode p1_StraightenKey = KeyCode.Period;
+	
+	private KeyCode p2_BurstOfSpeedKey = KeyCode.LeftShift;
+	private KeyCode p2_RepelKey = KeyCode.E;
+	private KeyCode p2_StraightenKey = KeyCode.Q;
+	
+	void Awake () 
+	{
 		head.gameObject.layer = LayerMask.NameToLayer("Player" + playerIndex);
 		defaultMoveForce = moveForce;
 	}
 	
-	void FixedUpdate () {
-		realtime = Time.realtimeSinceStartup;
-	
+	void FixedUpdate () 
+	{
+
 		//Burst of Speed Ability
 		if(burstOfSpeedEnabled || burstOfSpeedInUse)
 		{
-			bool shift = (playerIndex == 1 && Input.GetKey(KeyCode.RightShift)) || (playerIndex == 2 && Input.GetKey(KeyCode.LeftShift));
+			bool shift = (playerIndex == 1 && Input.GetKey(p1_BurstOfSpeedKey)) || (playerIndex == 2 && Input.GetKey(p2_BurstOfSpeedKey));
 			
 			if( shift && !burstOfSpeedInUse)
 			{
 				burstOfSpeedInUse = true;
 				burstOfSpeedEnabled = false;
-				Debug.Log("Burst of Speed Now in Use for" + playerIndex);
+				//Debug.Log("Burst of Speed Now in Use for" + playerIndex);
 			}
 			else if (burstOfSpeedInUse)
 			{
-				moveForce *= burstOfSpeedMultiplier;
-				if(moveForce >= burstOfSpeedMaxForce)
+				moveForce *= BurstOfSpeed.forceMultiplier;
+				if(moveForce >= BurstOfSpeed.maxForce)
 				{
 					moveForce = defaultMoveForce;
 					burstOfSpeedLastCastTime = Time.realtimeSinceStartup;
-					Debug.Log("Burst of Speed has completed for " +playerIndex);
+					//Debug.Log("Burst of Speed has completed for " +playerIndex);
 					burstOfSpeedInUse = false;
-					burstOfSpeedOnCooldown = true;
+					burstOfSpeedIsOnCooldown = true;
 				}
 			}
 		}
+		
+		//Repel Ability
+		if(repelEnabled)
+		{
+			//Debug.Log("Repel is ready.");	
+			bool repelButton = (playerIndex == 1 && Input.GetKey(p1_RepelKey)) || (playerIndex == 2 && Input.GetKey(p2_RepelKey));
+			
+			if (repelButton && !repelIsOnCooldown)
+			{
+				GameObject repelExplosion = Instantiate(Repel.repelExplosionPrefab , this.gameObject.transform.position, Quaternion.identity) as GameObject;
+				repelExplosion.GetComponent<RepelScript>().SetRepelAttributes( Repel.maxRange, Repel.maxForce, 
+																			   Repel.forceDecayMultiplier, Repel.growthMultiplier, playerIndex);
+				repelExplosion.GetComponent<RepelScript>().RepelIsArmed = true;
+				repelEnabled = false;
+				repelIsOnCooldown = true;
+				repelLastCastTime = Time.realtimeSinceStartup;
+				//Debug.Log("Repel has been used");
+			}
+		}
+		
+		//Straighten Ability
+		
 		
 		//Movement
 		bool left = (playerIndex == 1 && Input.GetKey(KeyCode.LeftArrow)) || (playerIndex == 2 && Input.GetKey(KeyCode.A));
@@ -81,21 +136,28 @@ public class PlayerScript : MonoBehaviour {
 		}
 		
 		//Check cooldowns
-		if (burstOfSpeedOnCooldown){
-			if (Time.realtimeSinceStartup - burstOfSpeedLastCastTime > burstOfSpeedCooldown)
+		if (burstOfSpeedIsOnCooldown){
+			if (Time.realtimeSinceStartup - burstOfSpeedLastCastTime > BurstOfSpeed.cooldownInSeconds)
 			{
 				burstOfSpeedEnabled = true;
-				Debug.Log("Burst of Speed is cooled down for" + playerIndex);
+				burstOfSpeedIsOnCooldown = false;
+				//Debug.Log("Burst of Speed is cooled down for" + playerIndex);
+			}
+		}
+		
+		if (repelIsOnCooldown){
+			if (Time.realtimeSinceStartup - repelLastCastTime > Repel.cooldown)
+			{
+				repelEnabled = true;
+				repelIsOnCooldown = false;
+				//Debug.Log("Repel is cooled down for " + playerIndex);
 			}
 		}
 		
 	}
 	
-	private void ToggleBurstOfSpeedEnabled() {
-		burstOfSpeedEnabled = !burstOfSpeedEnabled;
-	}
-	
-	private void ToggleBurstOfSpeedInUse() {
-		burstOfSpeedInUse = !burstOfSpeedInUse;
+	public int PlayerIndex
+	{
+		get{ return playerIndex;}
 	}
 }
